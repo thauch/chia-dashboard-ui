d8465950b971049168484a2fcd6589cb8d8a568f
NetMan <mail@netman.digital>
2021-05-21 00:33:49 +0200

    footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {faDiscord, faGithub, faPaypal} from "@fortawesome/free-brands-svg-icons";
+import {faDiscord, faTwitter, fabtc} from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-footer',
