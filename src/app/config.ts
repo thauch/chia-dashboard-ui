f754556135bc960cdfae8e8f0dd74573ef3c08db
Travis <hauch19990@gotoltc.edu>
2021-06-01 12:48:24 -0500

    Added global user/satellites to footer

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,9 +1,5 @@
-export const discordClientId = '846271886492565504';
-export const githubClientId = 'e6fec0a8b164e83c34e4';
-export const googleClientId = '249551392486-gofa7sgn1sec147avqua59o8mt9c54sk.apps.googleusercontent.com';
-export const apiBaseUrl = 'https://dev.chiadashboard.com/api';
+export const discordClientId = '';
+export const githubClientId = '';
+export const googleClientId = '';
+export const apiBaseUrl = 'https://us.chiadashboard.com/api';
 export const enablePeriodicUpdates = true;
-
-// DEV
-// export const githubClientId = 'f5cf9bfb6113a5fee63c';
-// export const apiBaseUrl = 'http://localhost:5000/api';
