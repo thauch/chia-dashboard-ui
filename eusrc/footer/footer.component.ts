80d01fbae3a010a46b41092ec3d65c782477f026
NetMan <mail@netman.digital>
2021-06-06 22:24:58 +0200

    Adjusted Differences

--- eusrc/footer/footer.component.ts
+++ eusrc/footer/footer.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {faDiscord, faTwitter, faBtc} from "@fortawesome/free-brands-svg-icons";
+import { Router } from '@angular/router';
+import {faDiscord, faGithub, faPaypal } from "@fortawesome/free-brands-svg-icons";
+import { ApiService } from '../api.service';
+import { StateService } from '../state.service';
+
 
 @Component({
   selector: 'app-footer',
@@ -13,6 +17,11 @@ export class FooterComponent implements OnInit {
   public faBtc = faBtc;
 
   ngOnInit() {
+   await this.stateService.init();
+    await this.stateService.getGlobalStats();
   }
 
+  get globalStats() {
+    return this.stateService.globalStats;
+  }
 }
