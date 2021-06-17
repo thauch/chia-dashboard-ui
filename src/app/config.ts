e057e0f205c5c519d0f4d192d370d6f5a5a9eb75
thauch <travishauch@gmail.com>
2021-06-17 01:11:36 -0500

    Update config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
-export const discordClientId = '846271886492565504';
-export const githubClientId = 'e57928bb2bce8991ef79';
-export const googleClientId = '249551392486-gofa7sgn1sec147avqua59o8mt9c54sk.apps.googleusercontent.com';
+export const discordClientId = '';
+export const githubClientId = '';
+export const googleClientId = '';
 export const apiBaseUrl = 'https://dev.chiadashboard.com/api';
 
 export const enablePeriodicUpdates = true;
