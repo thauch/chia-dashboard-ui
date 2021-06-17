80b70d21e70eaa46fb2b31472de9025f5ce2237c
thauch <travishauch@gmail.com>
2021-06-16 22:44:55 -0500

    updated config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,7 +1,7 @@
-export const discordClientId = '846271886492565504';
-export const githubClientId = 'e57928bb2bce8991ef79';
-export const googleClientId = '249551392486-gofa7sgn1sec147avqua59o8mt9c54sk.apps.googleusercontent.com';
-export const apiBaseUrl = 'https://dev.chiadashboard.com/api';
+export const discordClientId = '';
+export const githubClientId = '';
+export const googleClientId = '';
+export const apiBaseUrl = 'https://us.chiadashboard.com/api';
 
 export const enablePeriodicUpdates = true;
 export const requestDiscordGuildPermission = true;
