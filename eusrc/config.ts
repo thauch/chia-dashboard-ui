45dcd32ddad69a8aee10a657f6ade86077d1e534
thauch <travishauch@gmail.com>
2022-02-05 22:24:53 -0600

    transition to single site

--- eusrc/config.ts
+++
@@ -1,9 +0,0 @@
-export const discordClientId = '844447585553219584';
-export const githubClientId = 'd95a81a44733ab87cf1c';
-export const googleClientId = '414414818697-5neg9btq2ssqdlp3m7l9aitqt2vl6sq4.apps.googleusercontent.com';
-export const apiBaseUrl = 'https://eu.chiadashboard.com/api';
-export const enablePeriodicUpdates = true;
-
-// DEV
-// export const githubClientId = 'f5cf9bfb6113a5fee63c';
-// export const apiBaseUrl = 'http://localhost:5000/api';
